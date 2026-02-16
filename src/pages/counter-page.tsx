6bf497be0dbd986f4538e69e6db61e8df422235c
JinGyuGwak <wlsrb2695@naver.com>
2026-02-16 23:39:00 +0900

    [f/m] zustand 실습

--- src/pages/counter-page.tsx
+++ src/pages/counter-page.tsx
@@ -1,16 +1,13 @@
-import { Button } from "@/components/ui/button";
+import Controller from "@/components/counter/controller";
+import Viewer from "@/components/counter/viewer";
 import { useCountStore } from "@/store/count";
 
 export default function CounterPage() {
-  const store = useCountStore();
   return (
     <div>
       <h1 className="text-2xl font-bold">Counter</h1>
-      <div>{store.count}</div>
-      <div>
-        <Button onClick={store.decrease}>-</Button>
-        <Button onClick={store.increase}>+</Button>
-      </div>
+      <Viewer />
+      <Controller />
     </div>
   );
 }
